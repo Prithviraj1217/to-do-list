ab2837873894335b34293e2fd13caab29833055c
PRITHVIRAJ\prith <cprithvir72@gmail.com>
2024-10-10 15:26:29 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import Food from "./Food.jsx";
+import Card from './Card.jsx';
+import Button from './Button.jsx';
+import ToDoList from "./ToDoList.jsx";
+
+function App() {
+  return (
+    <>
+      {/* <Card />
+      <Card /> */}
+
+      {/* <Button /> */}
+      {/* <Header />
+      <Food />
+      <Food />
+      <Footer /> */}
+      <ToDoList />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Card.jsx
@@ -0,0 +1,14 @@
+
+function Card(){
+
+    return (
+        <div className="card">
+            <img className="card-image" src="https://via.placeholder.com/150" alt="prfile picture" />
+            <h2 className="card-title">Prithviraj</h2>
+            <p className="card-text" >Software Engineer</p>
+
+        </div>
+    );
+}
+
+export default Card
